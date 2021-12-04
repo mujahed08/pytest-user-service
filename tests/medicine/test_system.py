619ed1d96a8a0dd85e8ff7db7eacf307e28d4b18
Mohammed Mujahed Khan <mujahed08@hotmail.com>
2021-12-04 11:03:18 +0530

    medicine microservice integration pytest create

---
+++ tests/medicine/test_system.py
@@ -0,0 +1,20 @@
+import json
+import requests
+from tests.commons.logger import get_logger
+
+logger = get_logger('medicine-system-test_system.py')
+
+def test_medicine_create():
+
+    medicine = {
+        "type" : "Tab. ",
+        "name" : "Calpol",
+        "generic" : "Paracetamol",
+        "strength" : ["250 mg", "500 mg"],
+        "app_id" : 100
+    }
+    logger.info(medicine)
+    response = requests.post("http://127.0.0.1:8030/medicine-service-system/medicines", 
+        data=json.dumps(medicine))
+    logger.info(response.json())
+    assert response.status_code == 200
